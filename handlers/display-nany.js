13208a90e82121845621742eb532cc26906da0de
ShtankoDaria <daria.shtanko@gmail.com>
2020-03-10 15:46:08 +0100

    NaNy

--- handlers/display-nany.js
+++ handlers/display-nany.js
@@ -1,7 +1,15 @@
 function nanyHandler() {
   debugger;
   // write me!
+  strings.displayed = "NaNy";
+  const currentDisplayed = strings.displayed;
+  const currentArray = strings[currentDisplayed];
 
+  const currentListComponent = renderList(currentArray, currentDisplayed);
+
+  const listContainer = document.getElementById('selected');
+  listContainer.innerHTML = '';
+  listContainer.appendChild(currentListComponent);
   // log user interaction: handler name, user input, new state
   log.push({
     handler: 'NaNy',
