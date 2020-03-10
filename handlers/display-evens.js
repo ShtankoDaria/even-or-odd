49feafe7fef853619dd30f02bbf9d4e529fb0c5b
ShtankoDaria <daria.shtanko@gmail.com>
2020-03-10 15:35:49 +0100

    Odds handler + save handler

--- handlers/display-evens.js
+++ handlers/display-evens.js
@@ -1,7 +1,15 @@
 function evensHandler() {
   debugger;
   // write me!
+  strings.displayed = "evens";
+  const currentDisplayed = strings.displayed;
+  const currentArray = strings[currentDisplayed];
 
+  const currentListComponent = renderList(currentArray, currentDisplayed);
+
+  const listContainer = document.getElementById('selected');
+  listContainer.innerHTML = '';
+  listContainer.appendChild(currentListComponent);
   // log user interaction: handler name, user input, new state
   log.push({
     handler: 'evens',
--- handlers/display-odds.js
+++ handlers/display-odds.js
@@ -1,6 +1,15 @@
 function oddsHandler() {
   debugger;
   // write me!
+  strings.displayed = "odds";
+  const currentDisplayed = strings.displayed;
+  const currentArray = strings[currentDisplayed];
+
+  const currentListComponent = renderList(currentArray, currentDisplayed);
+
+  const listContainer = document.getElementById('selected');
+  listContainer.innerHTML = '';
+  listContainer.appendChild(currentListComponent);
 
   // log user interaction: handler name, user input, new state
   log.push({
